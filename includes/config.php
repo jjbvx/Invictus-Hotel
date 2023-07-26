fda6e152c766810a1b3b323080781df900605b53
jjbvx <cristo260823@gmail.com>
2023-07-25 21:11:56 -0600

    Correcion Dabatase

--- includes/config.php
+++ includes/config.php
@@ -7,10 +7,10 @@
 */
 
 //Database Constants
-defined('DB_SERVER') ? null : define("DB_SERVER","localhost");//define our database server
-defined('DB_USER') ? null : define("DB_USER","root");		  //define our database user	
-defined('DB_PASS') ? null : define("DB_PASS","");			  //define our database Password	
-defined('DB_NAME') ? null : define("DB_NAME","dragonhousedb"); //define our database Name
+defined('DB_SERVER') ? null : define("DB_SERVER","3306");//define our database server
+defined('DB_USER') ? null : define("DB_USER","fazt");		  //define our database user	
+defined('DB_PASS') ? null : define("DB_PASS","fastpassword");			  //define our database Password	
+defined('DB_NAME') ? null : define("DB_NAME","BD_UNION"); //define our database Name
 
 $thisFile = str_replace('\\', '/', __FILE__);
 $docRoot =$_SERVER['DOCUMENT_ROOT'];
